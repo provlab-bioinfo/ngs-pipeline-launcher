5049b9da30d83483a77bd4e1d460442a3ff275e1
knacko <geekbike@hotmail.com>
2023-07-31 14:39:50 -0600

    refactor: remove dependancies

--- moveToAPLGenomics.py
+++ moveToAPLGenomics.py
@@ -1,8 +1,5 @@
-import pandas as pd, os, searchTools as st, shutil, io, time, fileinput, subprocess, argparse, tempfile, pathlib
-from configparser import ConfigParser
+import os, shutil, time, argparse, glob
 from datetime import datetime
-pd.options.mode.chained_assignment = None  # default='warn'
-os.chdir(os.path.dirname(__file__))
 
 def isRunCompleted(path:str, seqType: str):
     """Checks whether a sequencing run is completed. 
@@ -11,7 +8,7 @@ def isRunCompleted(path:str, seqType: str):
     :param path: The path to the experiment directory
     :param seqType: The type of sequencing. Either 'Illumina' or 'Nanopore'
     :return: True if complete, False if not
-    """    
+    """
     if not os.path.exists(path):
         raise Exception("Run directory does not exist")
 
@@ -21,15 +18,14 @@ def isRunCompleted(path:str, seqType: str):
     elif seqType.lower() == "illumina":
         file = "CompletedJobInfo.xml"
 
-    found = st.findFile(os.path.join(path,"**",file))
+    found = glob.glob(os.path.join(path,"**",file), recursive = True)
 
     if (len(found)):
-        print(f"   Found {file} at {found}")
+        print(f"{datetime.now().strftime('%H:%M:%S')} | Found {file} at {found}. Starting move...")
         return True
     else:
         return False
 
-# Import the sample sheet
 parser = argparse.ArgumentParser(description='APL NGS File mover')
 parser.add_argument("-p", "--path", help="Path to the sequencing output folder")
 parser.add_argument("-d", "--destination", help="Path to the experiment directory on APL Genomics")
@@ -39,10 +35,14 @@ path = args.path
 dest = args.destination
 type = args.type
 
+print(f"{datetime.now().strftime('%H:%M:%S')} | Automated file transfer tool started", flush=True)
+
 # Check if run is finished sequencing
 while not isRunCompleted(path, type):
-    print("Waiting... ({})".format(datetime.now().strftime("%H:%M:%S")), flush=True)
+    print(f"{datetime.now().strftime('%H:%M:%S')} | Waiting...", flush=True)
     time.sleep(15)#*60)
 
-shutil.copytree(path, dest)
+shutil.copytree(path, dest, dirs_exist_ok=True)
+
+print(f"{datetime.now().strftime('%H:%M:%S')} | Transfer Completed", flush=True)
 
