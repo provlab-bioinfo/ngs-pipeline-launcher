684ee31cdebc30c44643fe0e11f8680f9d263faa
knacko <geekbike@hotmail.com>
2026-02-06 14:10:06 -0700

    feat: compiles to package

---
+++ ngs_pipeline_launcher/pipelineLauncher.py
@@ -0,0 +1,340 @@
+import pandas as pd, os, search_tools as st, shutil, io, time, subprocess, argparse, tempfile, pathlib, glob, re, glob, re, itertools
+from configparser import ConfigParser
+import openpyxl as xl
+from datetime import datetime
+from .runStatus import *
+pd.options.mode.chained_assignment = None  # default='warn'
+os.chdir(os.path.dirname(__file__))
+
+defaultSampleSheet = "./"
+SLURM = "/nfs/APL_Genomics/apps/production/ngs-pipeline-launcher/templates/SLURM_template.batch"
+barcodeCol = "Barcode"
+samplePosCol = "Sample_Pos"
+symlinkFQ = False
+
+def getSampleSheetDataVars(path:str, section:str):
+    """Generates a dictionary from the first two columns of a [HEADER] section
+    :param path: The path to the reference file
+    :param section: The name of the section
+    :return: A dictionary containing variable keys
+    """    
+    cfg = ConfigParser(allow_no_value=True)
+    cfg.optionxform = str
+    cfg.read(path)
+    dict = {k:v for k, *v in map(lambda x: str.split(x,sep=","), cfg[section])}
+    dict = {k:v[0] for k, v in dict.items() if v} # Removes blank keys and keep only first column after var
+    return (dict)
+
+def getSampleSheetDataFrame(path:str, section:str):
+    """Generates a DataFrame from a [HEADER] section
+    :param path: The path to the reference file
+    :param section: The name of the section
+    :return: A DataFrame representing the sections
+    """ 
+    cfg = ConfigParser(allow_no_value=True)
+    cfg.optionxform = str
+    cfg.read(path)
+    buf = io.StringIO()
+    buf.writelines('\n'.join(row.rstrip(',') for row in cfg[section]))
+    buf.seek(0)
+    df = pd.read_csv(buf)
+    return (df)
+
+def subsetWorksheet(path:str, group:str, out_path:str):
+    """Subsets a worksheet to only include a specific Sample_Group  in the [SAMPLES] section
+    :param group: The group to look for in the column 'Sample_Group'
+    :param path: The output path of the subsetted pipeline worksheet
+    :return: A DataFrame representing the sections
+    """ 
+    wb = xl.load_workbook(path)
+    ws = wb.active
+    rows = list(ws.iter_rows(min_row=1, max_row=ws.max_row))
+    for row in reversed(rows): 
+        cell = row[2] # col idx 3 is Sample_Group
+        if cell.value == "Sample_Group":
+            break
+        if cell.value != group:
+            ws.delete_rows(cell.row, 1)
+    wb.save(out_path)
+
+def generateSLURM(SLURM:str, jobName: str, runName: str, outputDir: str, command: str, email: str = None):
+    """Generates a SLURM command file based on a template
+    :param SLURM: Path to the template SLURM file
+    :param jobName: The name of the job
+    :param outputDir: The directory for output/error files
+    :param command: The path to the new SLURM file
+    """
+    file = open(SLURM, "rt")
+    data = file.read()
+    file.close()
+    data = data.replace("[JOB_NAME]", jobName)
+    data = data.replace("[OUTPUT_DIR]", os.path.join(outputDir,runName+"_out.txt"))
+    data = data.replace("[ERROR_DIR]", os.path.join(outputDir,runName+"_error.txt"))
+    data = data.replace("[EMAIL]",  "NONE" if email is None else email)
+    data = data.replace("[MAIL_TYPE]", "NONE" if email is None else "ALL")      
+    data = data.replace("[RUN_DIR]", outputDir)
+    outFile = os.path.join(outputDir,runName+"_SLURM.batch")
+    file = open(outFile, "wt+")
+    file.write(data)
+    file.write(f"\n\ncd {outputDir}")
+    file.write("\n\n"+command)
+    file.close()
+    return(outFile)
+
+def runLauncher(sampleSheetPath: str, email: str = None):
+
+    print(f"{currentTime()} | Pipeline launcher initialized")
+
+    print(f"{currentTime()} | Checking for sequencing completion file in '{sampleSheetPath}'...", flush=True)
+
+    # Check if run is finished sequencing
+    while not (completionFiles := isRunCompleted(sampleSheetPath)):#isRunCompleted(basePath, header["Seq_Type"]):
+        print(f"{currentTime()} |    Waiting...", flush=True)
+        time.sleep(15)#*60)
+
+    print(f"{currentTime()} | Found {completionFiles}.", flush=True)
+
+    # Read data from the sample sheet
+    print(f"{currentTime()} | Checking for pipeline worksheet...", flush=True)
+    with tempfile.NamedTemporaryFile() as sampleSheet:
+        os.chdir(sampleSheetPath) # TODO: This is gross
+        file = st.findFiles2(os.path.join(sampleSheetPath,"**","*PipelineWorksheet*"))
+        if not isinstance(file, list): file = [file]
+        file = [ f for f in file if "~$" not in f ] # Handle temporary file if open in 
+        if (len(file) == 0):
+            raise Exception(f"No pipeline worksheet found. Please check '{sampleSheetPath}'.")
+        if (len(file) > 1):
+            raise Exception(f"More than one pipeline worksheet identified. Found:\n{file}.")
+        
+        print(f"{currentTime()} | Found {file}.", flush=True)
+
+        file = file[0]
+        if pathlib.Path(file).suffix.lower() == ".xlsx":
+            df = pd.read_excel(file)
+            sampleSheetPath = sampleSheet.name
+            df.to_csv(sampleSheetPath, index=False)
+        else: sampleSheetPath = file
+
+        header = getSampleSheetDataVars(sampleSheetPath, "Header") 
+        runName = header["Run_Name"].strip()
+        runDir = header["Run_Dir"].strip()
+        seqType = header["Seq_Type"].lower().strip()
+        baseCall = header["Base_Call"].lower().strip()
+            
+        pipelines = getSampleSheetDataVars(sampleSheetPath, "Pipelines")
+        directories = getSampleSheetDataVars(sampleSheetPath, "Directories")   
+        directories = {group: os.path.join(dir,runName) for group, dir in directories.items()} # Adds path name to end of directory path
+        allSamples = getSampleSheetDataFrame(sampleSheetPath, "Samples")
+
+    # Check for appropriate inputs
+    if seqType not in ["nanopore","illumina"]:
+        raise Exception(f"Seq_Type must be either 'Nanopore' or 'Illumina' in the pipeline worksheet. Found: {header['Seq_Type']}")
+
+    if baseCall not in ["yes","no"]:
+        raise Exception(f"Base_Call must be either 'Yes' or 'No' in the pipeline worksheet. Found: {header['Base_Call']}")
+
+    if not os.path.isdir(runDir):
+        raise Exception(f"Run directory does not exist. Found: {header['Run_Dir']}")
+
+    groups = sorted(set(allSamples["Sample_Group"].dropna().values))
+    for group in groups:
+
+        if group == "ignore":
+            continue
+        
+        if not os.path.exists(pipelines[group]):
+            if (pipelines[group] != "ignore"):
+                raise Exception(f"Pipeline for '{group}' does not exist at '{pipelines[group]}'")
+
+        if os.path.exists(directories[group]):
+            if (directories[group] != "ignore"):
+                if (len(directories[group]) != 0):
+                    raise Exception(f"Directory for '{group}' at '{directories[group]}' already exists and is not empty. Please choose empty or non-existing directory.")
+
+    # time.sleep(15*60) # Extra wait to make sure everything is done
+
+    # Add barcodes to the respective sequencing type
+    allSamples[samplePosCol] = allSamples[barcodeCol]
+    allBarcodes = range(1,1000)
+
+    if (seqType == "nanopore"):
+        label = lambda x: f"_barcode{x}" if int(x) > 10 else f"_barcode0{x}"
+    elif (seqType == "illumina"):
+        label = lambda x: f"{x}_S"
+        
+    allSamples[barcodeCol] = allSamples[barcodeCol].apply(label)
+    allBarcodes = [label(barcode) for barcode in allBarcodes]
+
+    # Split sequencing run into respective folders
+    for group in groups:
+        if group.lower() == "ignore": continue
+
+        # Check for directory
+        outDir = directories[group]
+        ignore = False
+        try: ignore = outDir.lower() == "ignore"
+        except KeyError: ignore = True
+        if (ignore): print(f"{currentTime()} |    Ignoring file copy for {group}"); continue
+
+        # Move files
+        print(f"{currentTime()} |    Moving {group} to {outDir}", flush=True)
+        includeSamples = allSamples.loc[allSamples['Sample_Group'] == group][barcodeCol].values.tolist()
+        includeSamples = st.sortDigitSuffix(list(includeSamples))
+        print(f"{currentTime()} |       Extracting barcodes: " + ", ".join(st.collapseNumbers(includeSamples)), flush=True)
+        excludeSamples = list(set(allBarcodes) - set(includeSamples))
+        excludeSamples = st.sortDigitSuffix(list(excludeSamples))
+        # print("      Excluding barcodes: " + ", ".join(st.collapseNumbers(excludeSamples)), flush=True)
+        excludeSamples = [f"\/{sample}|\/.*_{sample}|\/.*-{sample}" for sample in excludeSamples]
+        excludeSamples = excludeSamples + ["fail","skip","unclassified","Undetermined","~$","pod5"]
+        excludeSamples = "|".join(excludeSamples)
+
+        pathfilter = ["**/*fastq.gz","**/report_*.json","**/*PipelineWorksheet*"]  
+
+        fileCount = 0    
+
+        for f in pathfilter:
+            for p in glob.glob(f, recursive=True, root_dir=runDir):
+                if os.path.isfile(os.path.join(runDir, p)) and not re.search(excludeSamples, p):   
+                    p_dest = p if (group != "PulseNet") else os.path.basename(p)
+                    os.makedirs(os.path.join(outDir, os.path.dirname(p_dest)), exist_ok=True)
+                    src = os.path.join(runDir, p)
+                    dst = os.path.join(outDir, p_dest)
+                    if symlinkFQ and pathlib.Path(p).suffix.lower() in [".fastq", ".fq"]:
+                        os.symlink(src, dst)
+                    else:
+                        shutil.copy(src, dst)
+                    fileCount = fileCount + 1
+        print(f"{currentTime()} |       Copied files: {fileCount}", flush=True)
+        
+        #subsetWorksheet(file, group, os.path.join(outDir,os.path.basename(file)))
+        
+    # Setup pipeline
+    print(f"{currentTime()} | Configuring pipelines...", flush=True)
+    for group in groups:
+        if group.lower() == "ignore": continue
+
+        # Check for pipeline
+        ignore = False
+        try: ignore = pipelines[group].lower() == "ignore"
+        except KeyError: ignore = True
+        if (ignore): print(f"{currentTime()} |    Ignoring pipeline for {group}"); continue
+
+        # Check output dir exists
+        if not os.path.exists(directories[group]):
+            print(f"{currentTime()} |    Output directory does not exist for {group}. Skipping.")
+            continue
+
+        print(f"{currentTime()} |    Generating SLURM for {group}...", flush=True)
+
+        # Parse controls
+        samples = allSamples.loc[allSamples['Sample_Group'] == group]
+        ctrls = samples.dropna(subset=['Control'])
+        negCtrls = posCtrls = pd.DataFrame()
+        samples = samples[samples['Control'].isna()]
+
+        if(len(ctrls.index)):
+            negCtrls = ctrls['Control'].str.lower() == "negative"
+            negCtrls = ctrls.loc[negCtrls==True]
+            negCtrls = ",".join(map(str,negCtrls[samplePosCol].values.tolist()))
+
+            posCtrls = ctrls['Control'].str.lower() != "negative"
+            posCtrls = ctrls.loc[posCtrls==True]
+            posCtrls["Control"] = posCtrls[samplePosCol].astype(str) +","+ posCtrls["Control"].astype(str)
+            posCtrls = " ".join(posCtrls["Control"].values.tolist())
+
+        # Parse extra data
+        accessions = allSamples.loc[allSamples['Sample_Group'] == group]
+
+        # Create the SLURM command
+        symlink = lambda dir,link: f"ln -s {st.findFiles2(os.path.join('./**',dir))[0]} {os.path.join(directories[group],link)}"
+
+        def symlink(dir,link): # Creates symlink command
+            path = os.path.join(directories[group],"**",dir)
+            file = st.findFiles2(path)
+            if (len(file) < 1): 
+                raise Exception(f"Error: No directory found for '{path}'")
+            elif(len(file) > 1):
+                raise Exception(f"Error: More than 1 directory found for '{path}'")
+            file = os.path.relpath(file[0],directories[group])
+            link = os.path.join(directories[group],link)
+            link = os.path.relpath(link,directories[group])
+            return f"ln -s {file} {link}"
+
+        commands = []
+        if (group == "ncov" or group == "ncov-ww"):
+            # Do symlinks
+            if seqType == "nanopore":
+                commands.append(symlink("fast5_pass","fast5"))   
+                commands.append(symlink("fastq_pass","gup_out"))   
+            elif seqType == "illumina":
+                commands.append(symlink("Fastq","fastq")) 
+
+            # Go to parent dir
+            parentDir = os.path.dirname(directories[group].rstrip("/")) + "/"
+            commands.append("\ncd {}\n".format(parentDir))
+
+            basecall = 1 if baseCall == "yes" else 2
+
+            baseDir = os.path.basename(directories[group].strip("/"))
+
+            # Run pipeline
+            command = f"python {pipelines[group]} -d {parentDir} -r {baseDir} -b {basecall}"
+            if (group == "ncov-ww"): command = command + " -f"
+
+            if len(posCtrls): command = f"{command} -p {posCtrls}"
+            if len(negCtrls): command = f"{command} -c {negCtrls}"
+            commands.append(command)
+
+        elif(group == "ncov-R10"):
+            baseDir = os.path.basename(directories[group].strip("/"))
+            commands.append(f"bash {pipelines[group]} {baseDir} {negCtrls}")
+
+        elif (group == "PulseNet"):
+            parentDir = os.path.dirname(directories[group].rstrip("/")) + "/"
+            baseDir = os.path.basename(directories[group].strip("/"))
+            commands.append("conda activate pulsenet_analysis_pipeline")               
+            commands.append(f"python {pipelines[group]} -d {parentDir} -r {baseDir}")
+
+        elif (group == "flu"):
+
+            commands.append(f"bash {pipelines[group]} {directories[group]}")
+
+            # commands.append("mkdir fastq")
+            # commands.append("for var in {{1..101}}; do rsync -avr */Alignment_1/*Fastq/$var\_*.fastq.gz fastq/; done")
+            # commands.append("cd fastq")
+            # commands.append(f"for var in *.gz; do mv $var {runName}_$var ; done")
+            # commands.append("cd ..")
+            # commands.append("prog_dir=/nfs/APL_Genomics/apps/production/influenza/influenza-pipeline")
+            # commands.append('for x in $(find -L ./fastq -name "*R1*.fastq.gz"); do name="${x/.\/fastq\//}"; bash ${prog_dir}/generate-influenza-consensus.txt --r1 $x --r2 ${x/_R1_/_R2_} --db ${prog_dir}/influenzaDB-2022-12-08/ --outdir results --prefix ${name/_R*/}; done')
+            # commands.append(f"cat results/*/*.tsv | perl -ne 'BEGIN{{$h=0;}}if(/^contig/){{if($h == 0){{$h = 1; print $_;}}else{{next;}}}}else{{print $_;}}' > ./results/{header['Run_Name']}.summary.tsv")
+            # commands.append('echo "Job finished with exit code $? at: `date`"')     
+
+        elif (group == "Strep"):
+            commands.append(f"bash {pipelines[group]} {runDir}")
+
+        else:
+            print(f"{currentTime()} |    No pipeline found for {group}. Running as bash script instead.", flush=True)
+            command = pipelines[group]
+
+        # Generate the SLURM file
+        SLURMfile = generateSLURM(SLURM = SLURM, 
+                                jobName = group+"_"+runName, 
+                                runName = runName, 
+                                outputDir = directories[group], 
+                                command = "\n".join(commands), 
+                                email = email)
+        out = subprocess.run(["sbatch",SLURMfile,"-v"], capture_output = True, text = True)
+        print(f"{currentTime()} |       {out.stdout}", flush=True)
+
+    print(f"{currentTime()} | All files transferred and pipeline initialized\n", flush=True)
+
+# Import the arguments
+parser = argparse.ArgumentParser(description='APL NGS Pipeline Launcher')
+parser.add_argument("-r", "--run", help="Path to the run directory. Must contain the PipelineWorksheet.xlsx.", default = defaultSampleSheet)
+parser.add_argument("-e", "--email", help="Notify status alerts by e-mail.", default = None)
+args = parser.parse_args()
+
+sampleSheetPath = args.run
+email = None if args.email == "None" else args.email
+
+runLauncher(sampleSheetPath, email)
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ngs-pipeline-launcher',
+    version='1.0.0',
+    packages=find_packages(exclude=['tests*']),
+    description='A launcher tool for sorting a sequencing run by pathogen and automatic initialization of their downstream pipelines.',
+    url='https://github.com/provlab-bioinfo/ngs-pipeline-launcher',
+    author='Andrew Lindsay',
+    author_email='andrew.lindsay@albertaprecisionlabs.ca',
+    include_package_data=True,
+    keywords=[],
+    zip_safe=False,
+    install_requires=[
+        'matplotlib',
+        'numpy=1.24.2',
+        'pandas=2.0.0',
+        'openpyxl',
+        'pip',
+        'setuptools',
+        'search-tools @ git+ssh://git@github.com/provlab-bioinfo/search-tools'
+    ],
+    python_requires='>=3.10, <4'
+)
