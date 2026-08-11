8dffc71caafdc4e783c23cfab172dcdf12f21aee
knacko <geekbike@hotmail.com>
2026-08-11 11:12:32 -0600

    refactor: added nodes to slurm launcher

--- ngs_pipeline_launcher/pipelineLauncher.sh
+++ ngs_pipeline_launcher/pipelineLauncher.sh
@@ -23,7 +23,7 @@ sbatch << EOT
 #SBATCH --mail-user=$EMAIL
 #SBATCH --output=$FOLDER"/"$NAME"_%x_out.txt"
 #SBATCH --error=$FOLDER"/"$NAME"_%x_error.txt"
-#SBATCH --partition=vm-cpu
+#SBATCH --partition=vm-cpu,big-ram,huge-ram
 
 #cd /nfs/APL_Genomics/scratch/
 python /nfs/APL_Genomics/apps/production/ngs-pipeline-launcher/scripts/pipelineLauncher.py -r $RUN -e $EMAIL
